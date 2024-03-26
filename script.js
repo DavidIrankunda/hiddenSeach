ae95ddd8b39bdf862f29c323dbe5a6e27f6241a4
DavidIrankunda <irankundadavid64@gmail.com>
2024-03-26 12:31:11 +0200

    "change event and body color"

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const search = document.querySelector('.search');
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 
-btn.addEventListener('mousemove' , () => {
+btn.addEventListener('click' , () => {
     search.classList.toggle('active')
     input.focus
 })
